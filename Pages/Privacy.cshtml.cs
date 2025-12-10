e0b5719086acd48b0676aa8b09f0b5beb8650020
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-12-10 19:10:41 +0000

    add initial .NET app

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace app_win_dotnet_sdc_krp_006_001.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
